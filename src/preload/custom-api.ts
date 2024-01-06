02d5610936be07a7ab527507d1df932fc661d358
potdig <potadig.ashnerd.zino@gmail.com>
2024-01-06 22:26:42 +0900

    format code

--- src/preload/custom-api.ts
+++ src/preload/custom-api.ts
@@ -7,9 +7,7 @@ type CustomAPI = {
 
 const customApi: CustomAPI = {
   play: () => ipcRenderer.invoke('play'),
-  openFolder: async () => {
-    return await ipcRenderer.invoke('openFolder')
-  }
+  openFolder: async () => await ipcRenderer.invoke('openFolder')
 }
 
 export { customApi }
