ed6251339519e72d23f5bb95efc8b9e71462f9dc
potdig <potadig.ashnerd.zino@gmail.com>
2024-01-10 15:57:30 +0900

    design

--- src/renderer/src/App.svelte
+++ src/renderer/src/App.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Close from 'svelte-material-icons/Close.svelte'
   import Controller from './components/Controller.svelte'
   import Playlist from './components/Playlist.svelte'
 
@@ -10,7 +11,7 @@
 <div class="container">
   <header>
     <p>Muffroove</p>
-    <button on:click={quit}>x</button>
+    <button on:click={quit}><Close width={24} height={24} /></button>
   </header>
 
   <main>
@@ -31,6 +32,9 @@
   }
 
   header {
+    font-size: 1.4em;
+    padding-block: 0.2em;
+    margin-bottom: 0.2em;
     position: relative;
     display: flex;
     flex-direction: row;
@@ -38,11 +42,24 @@
     align-items: center;
     user-select: none;
     -webkit-app-region: drag;
+    background-color: rgba($color: black, $alpha: 0.2);
 
     button {
       position: absolute;
+      display: flex;
+      justify-content: center;
+      align-items: center;
       right: 0;
       -webkit-app-region: no-drag;
+      width: 36px;
+      height: 100%;
+      background: none;
+      border: none;
+      color: inherit;
+
+      &:hover {
+        background-color: rgba($color: white, $alpha: 0.1);
+      }
     }
   }
 
--- src/renderer/src/components/Controller.svelte
+++ src/renderer/src/components/Controller.svelte
@@ -1,6 +1,21 @@
 <script lang="ts">
+  import FolderOpen from 'svelte-material-icons/FolderOpen.svelte'
+  import Pause from 'svelte-material-icons/Pause.svelte'
+  import Play from 'svelte-material-icons/Play.svelte'
+  import SkipBackward from 'svelte-material-icons/SkipBackward.svelte'
+  import SkipForward from 'svelte-material-icons/SkipForward.svelte'
+  import Stop from 'svelte-material-icons/Stop.svelte'
+  import VolumeHigh from 'svelte-material-icons/VolumeHigh.svelte'
+  import VolumeLow from 'svelte-material-icons/VolumeLow.svelte'
+
   import { volume } from '../stores/misc'
   import { controllable, currentIndex, currentMusic, musics, stateControl } from '../stores/musics'
+
+  const iconWidth = 24
+
+  $: volumeBackground = `linear-gradient(to right, #86a5b1 ${$volume * 100}%, rgba(0, 0, 0, 0) ${
+    $volume * 100
+  }%)`
 
   function next(): void {
     stateControl.set('next')
@@ -35,34 +50,85 @@
 
 <div id="controller">
   <div id="buttons">
-    <button on:click={prev} disabled={!$controllable}>&lt;&lt;</button>
-    <button on:click={stop} disabled={!$controllable}>[]</button>
-    <button on:click={play} disabled={!$controllable}>&gt;</button>
-    <button on:click={pause} disabled={!$controllable}>||</button>
-    <button on:click={next} disabled={!$controllable}>&gt;&gt;</button>
-    <button on:click={openFolder} disabled={!$controllable}>open folder</button>
+    <button on:click={prev} disabled={!$controllable}
+      ><SkipBackward width={iconWidth} height={iconWidth} /></button
+    >
+    <button on:click={stop} disabled={!$controllable}
+      ><Stop width={iconWidth} height={iconWidth} /></button
+    >
+    <button on:click={play} disabled={!$controllable}
+      ><Play width={iconWidth} height={iconWidth} /></button
+    >
+    <button on:click={pause} disabled={!$controllable}
+      ><Pause width={iconWidth} height={iconWidth} /></button
+    >
+    <button on:click={next} disabled={!$controllable}
+      ><SkipForward width={iconWidth} height={iconWidth} /></button
+    >
+    <button on:click={openFolder} disabled={!$controllable}
+      ><FolderOpen width={iconWidth} height={iconWidth} /></button
+    >
   </div>
   <div id="volume">
-    <input type="range" bind:value={$volume} min="0" max="1" step="0.01" />
+    <VolumeLow width={iconWidth} height={iconWidth} />
+    <input
+      type="range"
+      bind:value={$volume}
+      min="0"
+      max="1"
+      step="0.01"
+      style="color: red;background: {volumeBackground};"
+    />
+    <VolumeHigh width={iconWidth} height={iconWidth} />
   </div>
 </div>
 
 <style>
   #controller {
     width: 100%;
-    max-width: 300px;
+    max-width: 400px;
     display: flex;
     flex-direction: column;
     justify-content: center;
+    gap: 0.4em;
+    margin-bottom: 0.4em;
   }
 
   #buttons {
     display: flex;
     justify-content: space-between;
     gap: 8px;
+    margin-block: 0.2em;
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 48px;
+    height: 48px;
+    background: none;
+    color: inherit;
+    border-color: inherit;
+    border: 2px solid;
+    border-radius: 50%;
+  }
+
+  #volume {
+    display: flex;
+    gap: 0.4em;
+    width: 100%;
   }
 
   #volume input {
-    width: 100%;
+    flex: 1;
+    appearance: none;
+    background: none;
+    border: 1px solid;
+    border-color: #86a5b1;
+  }
+
+  #volume input::-webkit-slider-thumb {
+    opacity: 0;
   }
 </style>
